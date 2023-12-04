8fe5f4b00d460323a5f80167c7fd10661def6085
shahidullahmc <designer.shahidullah@outlook.com>
2023-12-04 01:09:01 -0500

    login update version

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,57 +1,102 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import './Login.css';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { API_URL } from '../../config';
 
 const Login = () => {
-    const handleClick = () => {
-        // Define your click handler logic here
-    };
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState('');
+  const navigate = useNavigate();
 
-    return (
-        <div className="container">
-            <div className="login-grid">
-                <div className="login-text">
-                    <h2>Login</h2>
-                </div>
-                <div className="login-text">
-                    Are you a new member? 
-                    <span>
-                        <Link to="/Sign_Up" style={{ color: '#2190FF' }}> Sign Up Here</Link>
-                    </span>
-                </div>
-                <br />
-                <div className="login-form">
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="email">Email</label>
-                            <input type="email" name="email" id="email" required pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}" className="form-control" placeholder="Enter your email" aria-describedby="helpId" />
-                            <small id="helpId" className="text-muted">Please enter a valid email address.</small>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password">Password</label>
-                            <input
-                                type="password"
-                                name="password"
-                                id="password"
-                                required className="form-control"
-                                placeholder="Enter your password"
-                                aria-describedby="helpId"
-                            />
-                        </div>
+  useEffect(() => {
+    if (sessionStorage.getItem("auth-token")) {
+      navigate("/");
+    }
+  }, []);
 
-                        <div className="btn-group">
-                            <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Login</button>
-                            <button type="reset" className="btn btn-danger mb-2 waves-effect waves-light">Reset</button>
-                        </div>
-                        <br />
-                        <div className="login-text">
-                            Forgot Password?
-                        </div>
-                    </form>
-                </div>
-            </div>
+  const login = async (e) => {
+    e.preventDefault();
+    
+    try {
+      const response = await fetch(`${API_URL}/api/auth/login`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          password: password,
+        }),
+      });
+
+      const json = await response.json();
+
+      if (json.authtoken) {
+        sessionStorage.setItem('auth-token', json.authtoken);
+        sessionStorage.setItem('email', email);
+
+        navigate('/');
+        window.location.reload();
+      } else {
+        if (json.errors) {
+          for (const error of json.errors) {
+            alert(error.msg);
+          }
+        } else {
+          alert(json.error);
+        }
+      }
+    } catch (error) {
+      console.error('Network error:', error);
+      // Handle network error, e.g., show an error message to the user
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="login-grid">
+        <div className="login-text">
+          <h2>Login</h2>
         </div>
-    );
-};
+        <div className="login-text">
+          Are you a new member? <span><Link to="/Sign_Up" style={{ color: '#2190FF' }}> Sign Up Here</Link></span>
+        </div>
+        <br />
+        <div className="login-form">
+          <form onSubmit={login}>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                name="email"
+                id="email"
+                className="form-control"
+                placeholder="Enter your email"
+                aria-describedby="helpId"
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                name="password"
+                id="password"
+                className="form-control"
+                placeholder="Enter your password"
+                aria-describedby="helpId"
+              />
+            </div>
+            <div className="btn-group">
+              <button type="submit" className="btn btn-primary mb-2 mr-1 waves-effect waves-light">Login</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
 
 export default Login;
